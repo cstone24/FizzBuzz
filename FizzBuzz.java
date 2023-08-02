d759de73e7e800ec6562d0f01c7c1e4ce9731781
cstone24 <97609348+cstone24@users.noreply.github.com>
2023-08-02 16:21:14 -0400

    Add files via upload

---
+++ FizzBuzz.java
@@ -0,0 +1,22 @@
+package fizzbuzz;
+import java.util.Scanner;
+
+public class FizzBuzz {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int number;
+		
+		System.out.print("Number: ");
+		number = input.nextInt();
+		
+		if (number % 5 == 0 && number % 3 == 0) 
+			System.out.println("FizzBuzz");
+		else if (number % 3 == 0)
+			System.out.println("Buzz");
+		else if (number % 5 == 0)
+			System.out.println("Fizz");
+		else
+			System.out.println(number);	
+	}
+
+}
